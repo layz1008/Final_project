f9a58056e4b3fe062e18ee68c1fd8b7276dbf83d
jozkan30 <jozkan2@gmail.com>
2023-02-08 16:47:03 -0500

    adds dummy data to home

--- final_project/src/screens/home/Home.jsx
+++ final_project/src/screens/home/Home.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./home.css"
-
+import Post from "../../components/posts/Post.jsx";
 export default function Home() {
  
   return (
     <div className="home-page-main-container">
-   <h1>I am the home page </h1>
-    </div>
+      <div className= "home-page-filter-container">
+        <p className="filter-text"> Filter By:</p>
+        <ul className="filter-buttons">
+          <li className="toggle-filter"> <button> Most Up Votes  👍</button></li>
+          <li className="toggle-filter"> <button> Most Down Votes 👎 </button></li>
+          <li className="toggle-filter"> <button> Trending 🔥 </button></li>
+        </ul>
+        </div>
+        <div className="home-page-post-container">
+          <Post  />
+          <Post  />
+          <Post  />
+          <Post  />
+          <Post  />
+        </div>
+      </div>
 
   )
 }
