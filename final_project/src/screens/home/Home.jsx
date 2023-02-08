6c222d6798cf7f8604764f72025e14f84845ac32
jozkan30 <jozkan2@gmail.com>
2023-02-08 17:19:26 -0500

    adds user-asset component

--- final_project/src/screens/home/Home.jsx
+++ final_project/src/screens/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import "./home.css"
+import "./home.css";
 import Post from "../../components/posts/Post.jsx";
+import UserAssets from "../../components/user_assets/UserAssets.jsx";
 export default function Home() {
  
   return (
@@ -12,6 +13,9 @@ export default function Home() {
           <li className="toggle-filter"> <button> Most Down Votes 👎 </button></li>
           <li className="toggle-filter"> <button> Trending 🔥 </button></li>
         </ul>
+        </div>
+        <div className="user-assets-container">
+        <UserAssets />
         </div>
         <div className="home-page-post-container">
           <Post  />
