fed2560f0c8d480ef571a24c903bd85bb741b9a7
kwonangela <kwonangelaa@gmail.com>
2023-02-09 10:08:01 -0500

    save progress for sign up

--- final_project/src/screens/signUp/SignUp.jsx
+++ final_project/src/screens/signUp/SignUp.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+// import user authentication 
 import "./signup.css";
 
 export default function SignUp() {
 
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    valid: ""
+  });
+
+  const [username, setUserName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordConfirm, setPasswordConfirm] = useState("")
+  const [valid, setValid] = useState("")
+  
     return(
   <div className="sign-up-main-container">
     <h1> Sign up page</h1>
