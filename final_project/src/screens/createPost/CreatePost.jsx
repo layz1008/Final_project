581878b1381d7089970552522cc540a198912f27
layz1008 <zlay1008@gmail.com>
2023-02-09 10:19:18 -0500

    js skeleton

--- final_project/src/screens/createPost/CreatePost.jsx
+++ final_project/src/screens/createPost/CreatePost.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+//import React from "react";
 import "./createpost.css"
+import React, { useState } from "react";
 
-export default function CreatePost() {
- 
+const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Submitting Title: ${title} and Text: ${text}`);
+    setTitle("");
+    setText("");
+  };
+
   return (
-    <div className="create-post-main">
-   <h1>This is where Posts are Created! </h1>
+    <div className="create-post">
+      <h2>Create a New Post</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Enter a title for your post"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="text">Text:</label>
+          <textarea
+            id="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter the text for your post"
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
+  );
+};
 
-  )
-}
+export default CreatePost;
