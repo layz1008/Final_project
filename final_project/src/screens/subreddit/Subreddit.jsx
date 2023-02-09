fc492ee902a97b70e55360da8efd8c31da4ad0ed
ValtierraXavier <xavier.valtierra@icloud.com>
2023-02-09 14:33:36 -0500

    add formatting

--- final_project/src/screens/subreddit/Subreddit.jsx
+++ final_project/src/screens/subreddit/Subreddit.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 import "./subreddit.css";
 import Post from "../../components/posts/Post";
+import { Link } from "react-router-dom";
 
 export default function Subreddit() {
+  
   return (
     <div className="home-page-main-container">
-      <h1>Sub - Title </h1>
+      
       
       <div id ='subredditHeader'>
-        <label for ='post-filters'className = 'drop-down-menu' id ='subreddit-banner-dropdownm-enu'>Filter:</label>
-        <select name = 'post-filters' id = 'postFilterOptions'>
-          <option value = 'trending'>Trending</option>
-          <option value = 'new'>New</option>
-          <option valuer = 'hot'>Hot</option>
-        </select>
+        <img id ='user-avatar' src="https://icon-library.com/images/generic-user-icon/generic-user-icon-10.jpg" alt='user avatar'></img>
+        <div id = 'title-subLink-join'>
+          <h1 id ='sub-title'>Cool Fish</h1>
+          <Link to ='/readmex2' id = 'sub-link'>r/coolfish</Link>
+        </div>
+        <button id = "join-button">Join</button>
+        <p id ='subreddit-description'>A really cool subreddit of really cool fishies</p>
+        <div id = 'filter-div'>
+          <label for ='post-filters'className = 'drop-down-menu' id ='subreddit-banner-dropdown-menu-label'>Post Filters:  </label>
+          <select name = 'post-filters' id = 'post-filter-options'>
+            <option value = 'trending'>Trending</option>
+            <option value = 'new'>New</option>
+            <option value = 'hot'>Hot</option>
+          </select>
+        </div>
       </div>
     
-    {/* if join-subreddit == Join clicked. check if user object contains subreddit-ID then change button to Joined */}
-      <button id = "join-subreddit">Join</button>
+    {/* if join-subreddit == Join clicked. check if user object contains subreddit-ID then change button to Joined if not display join */}
+     
       <Post/>
       <Post/>
       <Post/>
