928a1673c72a1d316fa246cd71bd8c7f84c1b7ad
Edward Olstrom <eddieolstrom1@gmail.com>
2023-02-10 15:05:28 -0500

    modal is complete just needs css

--- final_project/src/components/posts/PostModal.jsx
+++ final_project/src/components/posts/PostModal.jsx
@@ -1,4 +1,4 @@
-function PostModal({modalPost, displayModal, setDisplayModal}) {
+function PostModal({modalPost, displayModal, setDisplayModal, comments}) {
     return(
         <div className={displayModal? "postModal" : 'hideElement'}>
             <div className="closeBtnContainer">
@@ -8,6 +8,11 @@ function PostModal({modalPost, displayModal, setDisplayModal}) {
                 <div className="sub-count"> {modalPost.sub}</div>
                 <div className="votes"> {modalPost.up_votes}</div>
                 <div className="votes2"> {modalPost.down_votes}</div>
+                <div className="comments">
+                    {comments.filter(comment => comment.post == modalPost.id).map(comment => (
+                        <p key={comment.id}>{comment.comment}</p>
+                    ))}
+                </div>
                 <svg className="closepostModal" onClick={() => setDisplayModal(false)}>
                 <line x1='10' y1='12' x2='25' y2='12' stroke='#ffffff' strokeWidth='3' />
                 </svg>
