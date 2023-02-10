40c96fb1750416ecf2d2f6b61ec28bb329121ef8
layz1008 <zlay1008@gmail.com>
2023-02-10 14:07:20 -0500

    navbar buttons

--- final_project/src/components/posts/PostModal.jsx
+++ final_project/src/components/posts/PostModal.jsx
@@ -1,19 +1,40 @@
 function PostModal(props) {
-    return(
-        <div className={props.displayModal? "postModal" : 'hideElement'}>
-            <div className="closeBtnContainer">
-                <h1>Hello, we are a modal</h1>
-                <div className="modalTitle">{}</div>
-                <div clasName="commnets"> {}</div>
-                <div className="votes"> {}</div>
-                <div className="subreddit name"> {}</div>
-                <svg className="closepostModal" onClick={() => props.setDisplayModal(false)}>
-                <line x1='0' y1='12' x2='25' y2='12' stroke='rgb(100, 70, 0)' stroke-width='3' />
-                </svg>
-            </div>
- 
+    const { displayModal, setDisplayModal } = props;
+    const body = document.querySelector('body');
+  
+    const handleClose = () => {
+      setDisplayModal(false);
+      body.classList.remove('freeze-body');
+    };
+  
+    if (displayModal) {
+      body.classList.add('freeze-body');
+    }
+  
+    return (
+      <div className={displayModal ? 'postModal' : 'hideElement'}>
+        <div className="closeBtnContainer">
+          <h1 className="modalTitle">Hello, we are a modal</h1>
+          <div className="comments">{props.comments}</div>
+          <div className="votes">{props.votes}</div>
+          <div className="subredditName">{props.subredditName}</div>
+          <svg
+            className="closePostModal"
+            onClick={handleClose}
+          >
+            <line
+              x1="0"
+              y1="12"
+              x2="25"
+              y2="12"
+              stroke="rgb(100, 70, 0)"
+              stroke-width="3"
+            />
+          </svg>
         </div>
-    )
-}
-
-export default PostModal
+      </div>
+    );
+  }
+  
+  export default PostModal;
+  
