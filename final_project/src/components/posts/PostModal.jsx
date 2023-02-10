ed1ff7b9bf0f23aed782892b146418336ff5b961
kwonangela <kwonangelaa@gmail.com>
2023-02-10 10:40:33 -0500

    update post css

--- final_project/src/components/posts/PostModal.jsx
+++ final_project/src/components/posts/PostModal.jsx
@@ -1,14 +1,34 @@
+// function PostModal(props) {
+//     return(
+//         <div className={props.displayModal? "postModal" : 'hideElement'}>
+//             <div className="closeBtnContainer">
+//                 <h1>Hello, we are a modal</h1>
+//                 <div className="modalTitle">{}</div>
+//                 <div className="comments"> {}</div>
+//                 <div className="votes"> {}</div>
+//                 <div className="subreddit name"> {}</div>
+//                 <svg className="closepostModal" onClick={() => props.setDisplayModal(false)}>
+//                 <line x1='10' y1='12' x2='25' y2='12' stroke='#ffffff' strokeWidth='3' />
+//                 </svg>
+//             </div>
+ 
+//         </div>
+//     )
+// }
+
+// export default PostModal
+import { Modal } from "react-responsive-modal"
 function PostModal(props) {
     return(
         <div className={props.displayModal? "postModal" : 'hideElement'}>
             <div className="closeBtnContainer">
                 <h1>Hello, we are a modal</h1>
                 <div className="modalTitle">{}</div>
-                <div clasName="commnets"> {}</div>
+                <div className="comments"> {}</div>
                 <div className="votes"> {}</div>
                 <div className="subreddit name"> {}</div>
                 <svg className="closepostModal" onClick={() => props.setDisplayModal(false)}>
-                <line x1='0' y1='12' x2='25' y2='12' stroke='rgb(100, 70, 0)' stroke-width='3' />
+                <line x1='10' y1='12' x2='25' y2='12' stroke='#ffffff' strokeWidth='3' />
                 </svg>
             </div>
  
