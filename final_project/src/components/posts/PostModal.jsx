e01b15f8ceef6ab5bd96d67fc2242366e6e6ecea
layz1008 <zlay1008@gmail.com>
2023-02-13 16:29:16 -0500

    text editor fix

--- final_project/src/components/posts/PostModal.jsx
+++ final_project/src/components/posts/PostModal.jsx
@@ -1,4 +1,3 @@
-
 function PostModal({modalPost, displayModal, setDisplayModal, comments}) {
     const body = document.querySelector("body");
 
@@ -11,12 +10,13 @@ function PostModal({modalPost, displayModal, setDisplayModal, comments}) {
         body.classList.add('freeze-body');
     }
 
-    return(
-        <div className={displayModal? "post-modal" : 'hide-element'}>
+    return (
+        <div className={displayModal ? "post-modal" : 'hide-element'}>
             <div className="close-btn-container">
                 <div className="modal-title">{modalPost.title}</div>
                 <img src={modalPost.img_url} />
-                <div className="texts">{modalPost.text}</div>
+                <div className="texts">
+                <div dangerouslySetInnerHTML={{ __html: modalPost.text }} /></div>
                 <div className="sub-count"> {modalPost.sub}</div>
                 <div className="votes"> {modalPost.up_votes}</div>
                 <div className="votes2"> {modalPost.down_votes}</div>
@@ -25,14 +25,14 @@ function PostModal({modalPost, displayModal, setDisplayModal, comments}) {
                         <p key={comment.id}>{comment.comment}</p>
                     ))}
                 </div>
-                <svg className="close-post-modal" onClick= {handleClose}>
+                <svg className="close-post-modal" onClick={handleClose}>
                     <line x1='6' y1='30' x2='20' y2='10' stroke='#ffffff' strokeWidth='3' />
                     <line x1='6' y1='10' x2='20' y2='30' stroke='#ffffff' strokeWidth='3' />
                 </svg>
             </div>
- 
         </div>
     )
 }
 
 export default PostModal;
+
