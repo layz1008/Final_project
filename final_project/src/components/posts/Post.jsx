fb69cd5afc20473ef19c4c0237faf0169a6883c1
ValtierraXavier <xavier.valtierra@icloud.com>
2023-02-13 13:41:10 -0500

    show card metrics

--- final_project/src/components/posts/Post.jsx
+++ final_project/src/components/posts/Post.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import "./post.css";
+import { useState, useEffect } from "react";
+import { getComments, getComment } from "../../services/comments";
+import { getPost } from "../../services/posts";
 
 export default function Post({post, setModalPost, setDisplayModal}) {
+
+  const [commentCount, setCommentCount] = useState(0)
+  const [upvote, setUpvote] = useState(0)
+  const [downvote, setDownvote] = useState(0)
+  const [totalvote, setTotalvote] = useState(0)
+  
+  async function commentMetrics(){
+      let res = await getComments()
+      setCommentCount(res.length)
+      // setDownvote(res.down_votes)
+      // setUpvote(res.up_votes)
+      // console.log(res.id)
+    }
+
+    async function postMetrics(){
+      let res = await getPost(3)
+      let ups = res.up_votes
+      let downs = res.down_votes
+      let totes = (ups - downs)
+      setUpvote(ups)
+      setDownvote(downs)
+      setTotalvote(totes)
+    }
+  postMetrics()
+  commentMetrics()
 
   function handleClick() {
     console.log("Modal was clicked")
@@ -18,12 +46,12 @@ export default function Post({post, setModalPost, setDisplayModal}) {
         alt="123">
       </img>
       <div id = 'votes-div'>
-        <span className="votes" id = 'upVotes'> ⬆: 20 </span>
+        <span className="votes" id = 'upVotes'> ⬆: {upvote} </span>
         <br />
-        <span className ='votes'id = 'totalVotes'>+15</span>
+        <span className ='votes'id = 'totalVotes'>{totalvote}</span>
         <br />
-        <span className="votes" id ='downVotes'> ⬇: 5 </span>
-        <p id = 'comments-tab'> 💬 20 Comments </p>
+        <span className="votes" id ='downVotes'> ⬇: {downvote} </span>
+        <p id = 'comments-tab'> 💬 {commentCount} Comments </p>
       </div>
       
       {/* <p> Fish are great and cool as well. They are fun.</p> */}
