e4df390aad8515853577dcfa069f8e1757d16a0a
jozkan30 <jozkan2@gmail.com>
2023-02-14 10:14:41 -0500

    adds data to card component

--- final_project/src/components/posts/Post.jsx
+++ final_project/src/components/posts/Post.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import "./post.css";
 
-export default function Post({post, setModalPost, setDisplayModal}) {
-
+export default function Post({ post, setModalPost, setDisplayModal }) {
   function handleClick() {
-    console.log("Modal was clicked")
-    setModalPost(post)
-    setDisplayModal(true)
-    document.body.classList.add("no-scroll")
+    console.log("Modal was clicked");
+    setModalPost(post);
+    setDisplayModal(true);
+    document.body.classList.add("no-scroll");
   }
 
   return (
     <div onClick={handleClick} className="post-card">
       <h2>{post.title} </h2>
-      <img
-      src={`${post.img_url}`}
-      alt={`${post.id}`}
-      />
-
-      <div id = 'votes-div'>
-        <span className="votes" id = 'upVotes'> ⬆:{post.up_votes}  </span>
+      <img src={`${post.img_url}`} alt={`${post.id}`} />
+      <div id="votes-div">
+        <span className="votes" id="upVotes">
+          {" "}
+          ⬆:{post.up_votes}{" "}
+        </span>
         <br />
-        <span className="votes" id ='downVotes'> ⬇: {post.down_votes} </span>
-        <p id = 'comments-tab'> 💬  </p>
-      </div> 
+        <span className="votes" id="downVotes">
+          {" "}
+          ⬇: {post.down_votes}{" "}
+        </span>
+        <p id="comments-tab"> 💬 </p>
+      </div>
     </div>
   );
 }
--- final_project/src/screens/home/Home.jsx
+++ final_project/src/screens/home/Home.jsx
@@ -28,7 +28,7 @@ export default function Home() {
     };
 
     fetchComment();
-  }, []);
+  }, []); 
 
   return (
     <div className="home-page-main-container">
