79b813ec0c820a5d490166484470c43485865c4b
layz1008 <zlay1008@gmail.com>
2023-02-15 10:53:50 -0500

    Comments

--- final_project/src/components/posts/PostModal.jsx
+++ final_project/src/components/posts/PostModal.jsx
@@ -1,55 +1,80 @@
 import { useEffect, useRef, useState } from "react";
 import "./post.css";
+import { updatePost } from "../../services/posts";
 
 function PostModal({modalPost, displayModal, setDisplayModal, comments}) {
-    const body = document.querySelector("body");
-    const [upVotes, setUpVotes] = useState(modalPost.up_votes);
-    const [downVotes, setDownVotes] = useState(modalPost.down_votes);
+  const body = document.querySelector("body");
 
-    const handleUpVote = () => {
-        setUpVotes(upVotes + 1);
-    };
+  const handleClose = () => {
+    setDisplayModal(false);
+    body.classList.remove('freeze-body');
+  };
 
-    const handleDownVote = () => {
-        setDownVotes(downVotes + 1);
-    };
+  const [upVotes, setUpVotes] = useState(modalPost.up_votes);
+  const [downVotes, setDownVotes] = useState(modalPost.down_votes);
 
-    const handleClose = () => {
-        setDisplayModal(false);
-        body.classList.remove('freeze-body');
-    };
-
-    if (displayModal) {
-        body.classList.add('freeze-body');
+  const handleUpVote = async () => {
+    setUpVotes(prevUpVotes => prevUpVotes += 1);
+    const updatedPost = { ...modalPost, up_vote: upVotes += 1 };
+    try {
+      const response = await updatePost(modalPost.id, updatedPost);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
     }
+  };
+  
+  const handleDownVote = async () => {
+    setDownVotes(prevDownVotes => prevDownVotes += 1);
+    const updatedPost = { ...modalPost, down_vote: downVotes += 1 };
+    try {
+      const response = await updatePost(modalPost.id, updatedPost);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
 
-    return (
-        <div className={displayModal ? "post-modal" : 'hide-element'}>
-            <div className="close-btn-container">
-                <div className="modal-title">{modalPost.title}</div>
-                <img className='modal-image' src={modalPost.img_url} />
-                <div className="texts">
-                <div dangerouslySetInnerHTML={{ __html: modalPost.text }} /></div>
-                <div className="sub-count"> {modalPost.sub}</div>
-                <div className="votes-container">
-                <div className="votes"> {upVotes}</div>
-                    <button className="upvote-button" onClick={handleUpVote}>Up Vote</button>
-                    <div className="votes2"> {downVotes}</div>
-                    <button className="downvote-button" onClick={handleDownVote}>Down Vote</button>
-                </div>
-                <div className="comments">
-                    {comments.filter(comment => comment.post == modalPost.id).map(comment => (
-                        <p key={comment.id}>{comment.comment}</p>
-                    ))}
-                </div>
-                <svg className="close-post-modal" onClick={handleClose}>
-                    <line x1='6' y1='30' x2='20' y2='10' stroke='#ffffff' strokeWidth='3' />
-                    <line x1='6' y1='10' x2='20' y2='30' stroke='#ffffff' strokeWidth='3' />
-                </svg>
-            </div>
+
+  if (displayModal) {
+    body.classList.add('freeze-body');
+  }
+
+  return (
+    <div className={displayModal ? "post-modal" : 'hide-element'}>
+      <div className="close-btn-container">
+        <div className="modal-title">{modalPost.title}</div>
+        <img className='modal-image' src={modalPost.img_url} />
+        <div className="texts">
+          <div dangerouslySetInnerHTML={{ __html: modalPost.text }} />
         </div>
-    )
+        <div className="votes-container">
+          <div id="votes-div">
+            <div className="votes">{upVotes}</div>
+            <button className="upvote-button" onClick={handleUpVote}>Upvote</button>
+          </div>
+          <div id="votes-div">
+            <div className="votes2">{downVotes}</div>
+            <button className="downvote-button" onClick={handleDownVote}>Downvote</button>
+          </div>
+        </div>
+        <div className="comments-container">
+          <div className="comments-scrollable">
+            {comments.filter(comment => comment.post == modalPost.id).map(comment => (
+              <p key={comment.id}>{comment.comment}</p>
+            ))}
+          </div>
+        </div>
+        <svg className="close-post-modal" onClick={handleClose}>
+          <line x1='6' y1='30' x2='20' y2='10' stroke='#ffffff' strokeWidth='3' />
+          <line x1='6' y1='10' x2='20' y2='30' stroke='#ffffff' strokeWidth='3' />
+        </svg>
+      </div>
+    </div>
+  );
 }
 
-export default PostModal;
 
+
+export default PostModal;
