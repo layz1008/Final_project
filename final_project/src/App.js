a276aa561c108e412153209f803b71ce5704a530
kwonangela <kwonangelaa@gmail.com>
2023-02-15 12:12:56 -0500

    Saving before i mess things up

--- final_project/src/App.js
+++ final_project/src/App.js
@@ -4,23 +4,52 @@ import CreatePost from "./screens/createPost/CreatePost.jsx";
 import Subreddit from "./screens/subreddit/Subreddit.jsx";
 import SignUp from "./screens/signUp/SignUp.jsx";
 import { Routes, Route } from "react-router-dom";
-
-// const [subreddit, setSubreddit] = useState([]);
-
-// const handleSearch = (e) => {
-//   const {value} = e.target
-//   const results = subreddit.filter((sub) => {
-//     return sub.title.toLowerCase().includes(value.toLowerCase())
-//   })
-//   setSubreddit(results)
-// }
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [filteredPost, setFilteredPosts] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     const response = await getPosts();
+  //     setPosts(response);
+  //     setFilteredPosts(response);
+  //   };
+  //   fetchPosts();
+  // }, []);
+  useEffect(() => {
+    fetch("https://final-project-server-production.up.railway.app/")
+      .then((res) => res.json())
+      .then((data) => {
+        let postsData = data.map((post, index) => {
+          return {
+            title: post.title,
+            text: post.text,
+            img_url: post.img_url,
+            up_votes: post.up_votes,
+            down_votes: post.down_votes
+          };
+        });
+        setPosts(postsData);
+        setFilteredPosts(postsData);
+      });
+  }, []);
+
+  const handleSearch = (e) => {
+    console.log("searching")
+    const {value} = e.target
+    const results = posts.filter((post) => {
+      return post.title.toLowerCase().includes(value.toLowerCase())
+    })
+    setFilteredPosts(results)
+  }
+
   return (
     <div className="App">
       <nav>
-        <Nav />
-        {/* <Nav handleSearch={handleSearch}/> */}
+        {/* <Nav /> */}
+        <Nav handleSearch={handleSearch}/>
       </nav>
     <div>
       <Routes>
