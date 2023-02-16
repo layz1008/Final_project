ec734400c68994f53d25c768d2f31341fdd12d66
kwonangela <kwonangelaa@gmail.com>
2023-02-16 00:35:20 -0500

    saving css

--- final_project/src/components/navbar/Nav.jsx
+++ final_project/src/components/navbar/Nav.jsx
@@ -1,47 +1,25 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import "./nav.css"
+import "./nav.css";
 import SignInModal from "./SignIn.jsx";
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu.jsx";
 
-
 // export default function Nav({ search, handleSearch }) {
 export default function Nav() {
-
- 
-    return (
+  return (
     <div>
       <nav className="navBar">
         <HamburgerMenu />
         <NavLink className={"navBarInfo Link"} to="/">
           Home
         </NavLink>
-        {/* <div className="search">
-        <input
-          type="text"
-          className="nav-search-input"
-          onChange={handleSearch}
-        />
-        </div> */}
         <NavLink className={"navBarInfo Link"} to="/create">
-          Create Post 
+          Create Post
         </NavLink>
         <NavLink className={"navBarInfo"} id="navbar-buttons" to="/signup">
           Sign-Up
         </NavLink>
-
-      {/* <p onClick={this.handleModalOpen} className={"navBarInfo"} id="navbar-buttons" >
-            Sign-In
-          </p> */}
-        </nav>
-        {/* <SignInModal
-              modalOpen={state.modalOpen}
-              handleModalOpen={handleModalOpen}
-              handleSignIn={handleSignIn}
-           /> */}
+      </nav>
     </div>
-  )
+  );
 }
-
-
-
