a433fa6e876178c74a826dc081c1d45317236991
layz1008 <zlay1008@gmail.com>
2023-02-16 06:39:13 -0500

    Comments working

---
+++ final_project/src/components/posts/CommentBox.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./CommentBox.css";
+import { createComment } from "../../services/comments";
+
+function CommentBox({ postId }) {
+  const [commentText, setCommentText] = useState("");
+
+  const handleCommentChange = (event) => {
+    setCommentText(event.target.value);
+  };
+
+  const handleCommentSubmit = async (event) => {
+    event.preventDefault();
+    if (commentText.trim() === "") {
+      return;
+    }
+    const newComment = {
+      comment: commentText,
+      post: postId,
+      up_votes: 0,
+      down_votes: 0,
+      commenter: 1 // replace with the ID of the commenter if needed
+    };
+    try {
+      const createdComment = await createComment(newComment);
+      console.log("Comment created:", createdComment);
+      setCommentText("");
+      // add code to update the UI with the new comment if needed
+    } catch (error) {
+      console.error("Error creating comment:", error);
+    }
+  };
+
+  return (
+    <form className="comment-form" onSubmit={handleCommentSubmit}>
+      <textarea
+        className="comment-input"
+        value={commentText}
+        onChange={handleCommentChange}
+        placeholder="Add a comment..."
+      />
+      <button className="comment-submit" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default CommentBox;
--- final_project/src/components/posts/PostModal.jsx
+++ final_project/src/components/posts/PostModal.jsx
@@ -1,76 +1,131 @@
 import { useEffect, useRef, useState } from "react";
-import "./post.css";
+import "./PostModal.css";
 import { updatePost } from "../../services/posts";
+import CommentBox from "./CommentBox";
 
-function PostModal({modalPost, displayModal, setDisplayModal, comments}) {
-    const body = document.querySelector("body");
-  
-    const handleClose = () => {
-      setDisplayModal(false);
-      body.classList.remove('freeze-body');
-    };
-  
-    const [upVotes, setUpVotes] = useState(modalPost.up_votes);
-    const [downVotes, setDownVotes] = useState(modalPost.down_votes);
-  
-    const handleUpVote = async () => {
-      setUpVotes(prevUpVotes => prevUpVotes += 1);
-      const updatedPost = { ...modalPost, up_vote: upVotes += 1 };
-      try {
-        const response = await updatePost(modalPost.id, updatedPost);
-        console.log(response);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    
-    const handleDownVote = async () => {
-      setDownVotes(prevDownVotes => prevDownVotes += 1);
-      const updatedPost = { ...modalPost, down_vote: downVotes += 1 };
-      try {
-        const response = await updatePost(modalPost.id, updatedPost);
-        console.log(response);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    if (displayModal) {
-      body.classList.add('freeze-body');
+function PostModal({ modalPost, displayModal, setDisplayModal, comments }) {
+  const body = document.querySelector("body");
+
+  const handleClose = () => {
+    setDisplayModal(false);
+    body.classList.remove('freeze-body');
+  };
+
+  const [upVotes, setUpVotes] = useState(modalPost.up_votes);
+  const [downVotes, setDownVotes] = useState(modalPost.down_votes);
+
+  const handleUpVote = async () => {
+    const newUpVotes = upVotes + 1;
+    console.log("new up votes:", newUpVotes); 
+    setUpVotes(newUpVotes);
+    const updatedPost = { ...modalPost, up_votes: newUpVotes };
+    console.log("updated post:", updatedPost);
+    try {
+      const response = await updatePost(modalPost.id, updatedPost);
+      console.log("update post response:", response); 
+    } catch (error) {
+      console.log("update post error:", error);
     }
+  };
   
-    return (
-      <div className={displayModal ? "post-modal" : 'hide-element'}>
-        <div className="close-btn-container">
-          <div className="modal-title">{modalPost.title}</div>
-          <img className='modal-image' src={modalPost.img_url} />
-          <div className="texts">
-            <div dangerouslySetInnerHTML={{ __html: modalPost.text }} />
-          </div>
-          <div className="votes-container">
-            <div id="votes-div">
-              <div className="votes">{upVotes}</div>
-              <button className="upvote-button" onClick={handleUpVote}>Upvote</button>
-            </div>
-            <div id="votes-div">
-              <div className="votes2">{downVotes}</div>
-              <button className="downvote-button" onClick={handleDownVote}>Downvote</button>
-            </div>
-          </div>
-          <div className="comments-container">
-            <div className="comments-scrollable">
-              {comments.filter(comment => comment.post == modalPost.id).map(comment => (
-                <div key={comment.id} className="comment">{comment.comment}</div>
-              ))}
-            </div>
-          </div>
-          <svg className="close-post-modal" onClick={handleClose}>
-            <line x1='6' y1='30' x2='20' y2='10' stroke='#ffffff' strokeWidth='3' />
-            <line x1='6' y1='10' x2='20' y2='30' stroke='#ffffff' strokeWidth='3' />
-          </svg>
-        </div>
-      </div>
-    );
+  
+  const handleDownVote = async () => {
+    const newDownVotes = downVotes + 1;
+    setDownVotes(newDownVotes);
+    const updatedPost = { ...modalPost, down_votes: newDownVotes };
+    try {
+      const response = await updatePost(modalPost.id, updatedPost);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+
+  const handleAddComment = async (newComment) => {
+    try {
+      const response = await addComment(newComment);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addComment = async (newComment) => {
+    try {
+      const response = await fetch("/api/comments", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newComment),
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (displayModal) {
+    body.classList.add("freeze-body");
   }
 
-  export default PostModal
+  return (
+    <div className={displayModal ? "post-modal" : "hide-element"}>
+      <div className="close-btn-container">
+        <div className="modal-title">{modalPost.title}</div>
+        <img className="modal-image" src={modalPost.img_url} />
+        <div className="texts">
+          <div dangerouslySetInnerHTML={{ __html: modalPost.text }} />
+        </div>
+        <div className="votes-container">
+          <div id="votes-div">
+            <div className="votes">{upVotes}</div>
+            <button className="upvote-button" onClick={handleUpVote}>
+              Upvote
+            </button>
+          </div>
+          <div id="votes-div">
+            <div className="votes2">{downVotes}</div>
+            <button className="downvote-button" onClick={handleDownVote}>
+              Downvote
+            </button>
+          </div>
+        </div>
+        <div className="comments-container">
+          <div className="comments-title">Comments:</div>
+          <div className="comments-scrollable">
+            {comments
+              .filter((comment) => comment.post === modalPost.id)
+              .map((comment) => (
+                <p key={comment.id} className="comment">{comment.comment}</p>
+              ))}
+          </div>
+        </div>
+        <CommentBox postId={modalPost.id} addComment={handleAddComment} />
+        <svg className="close-post-modal" onClick={handleClose}>
+          <line
+            x1="6"
+            y1="30"
+            x2="20"
+            y2="10"
+            stroke="#ffffff        "
+            strokeWidth="3"
+          />
+          <line
+            x1="6"
+            y1="10"
+            x2="20"
+            y2="30"
+            stroke="#ffffff"
+            strokeWidth="3"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+export default PostModal;
+
